8e2fd593ad4771e2a7b1ded95ef4674eeb1ddeac
Jeremy <JeremyJiang430@outlook.com>
2023-08-22 01:07:42 +0900

    chore: shell format

--- model/download_models.sh
+++ model/download_models.sh
@@ -1,37 +1,18 @@
 #!/bin/bash
 
-model_url="https://huggingface.co/bert-base-uncased"
-model_name="bert-base-uncased"
-if [ ! -r "$model_name" ]; then
-    echo "Download ${model_name} from ${model_url}"
-    git lfs clone ${model_url} 
-else
-    echo "${model_name} already exists."
-fi
+MODELS=(
+    "BAAI/bge-base-zh"
+    "BAAI/bge-large-zh"
+)
 
-model_url="https://huggingface.co/bert-large-uncased"
-model_name="bert-large-uncased"
-if [ ! -r "$model_name" ]; then
-    echo "Download ${model_name} from ${model_url}"
-    git lfs clone ${model_url} 
-else
-    echo "${model_name} already exists."
-fi
 
-model_url="https://huggingface.co/sentence-transformers/bert-base-nli-mean-tokens"
-model_name="bert-base-nli-mean-tokens"
-if [ ! -r "$model_name" ]; then
-    echo "Download ${model_name} from ${model_url}"
-    git lfs clone ${model_url} 
-else
-    echo "${model_name} already exists."
-fi
-
-model_url="https://huggingface.co/sentence-transformers/bert-large-nli-mean-tokens"
-model_name="bert-large-nli-mean-tokens"
-if [ ! -r "$model_name" ]; then
-    echo "Download ${model_name} from ${model_url}"
-    git lfs clone ${model_url} 
-else
-    echo "${model_name} already exists."
-fi
+for model in "${MODELS[@]}"; do
+    model_url="https://huggingface.co/${model}"
+    model_name="${model##*/}"
+    if [ ! -r "$model_name" ]; then
+        echo "Download ${model_name} from ${model_url}"
+        git lfs clone "${model_url}"
+    else
+        echo "${model_name} already exists."
+    fi
+done
